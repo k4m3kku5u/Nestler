db208dbfe42cf14768228318132cb4836daa74bc
k4m3kku5u <70122910+k4m3kku5u@users.noreply.github.com>
2022-03-18 20:11:14 -0500

    Update bot.py

--- utils/bot.py
+++ utils/bot.py
@@ -2,9 +2,9 @@ from utils.config import COGS
 from discord import *
 from discord.ext import commands
 from dotenv import load_dotenv
-import os
 
 client = commands.Bot(command_prefix=":")
+token = load_env("CLIENT_TOKEN")
 intents = Intents().default()
 intents.all()
 
@@ -12,7 +12,7 @@ async def init():
     async with client:
         for ext in COGS:
             await client.load_extension(ext)
-        await client.start(os.getenv("CLIENT_TOKEN"))
+        await client.start(token)
 
 @client.event
 async def on_ready():
