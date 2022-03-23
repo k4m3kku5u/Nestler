5b162e2feea46bb5ab18800b2bf076fc5effa61c
k4m3kku5u <70122910+k4m3kku5u@users.noreply.github.com>
2022-03-23 17:52:45 -0500

    Update general.py

--- cogs/general.py
+++ cogs/general.py
@@ -9,7 +9,7 @@ class general(commands.Cog):
     
     @app_commands.command(name="ping", description="A general utility to fetch latency times or test commands.")
     @app_commands.guilds(919846932459974686)
-    async def ping(self, interaction: Interaction)
+    async def ping(self, interaction: Interaction):
         embed = pingEmbed()
         embed.set_author(name=f'{interaction.user.name}', icon_url=interaction.user.avatar)
         embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/919847648758992927/953452806872911892/the_nest.jpg")
