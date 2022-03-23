bf33fe02021c5bf2ff4233884bc50077576e9ab2
k4m3kku5u <70122910+k4m3kku5u@users.noreply.github.com>
2022-03-23 17:39:20 -0500

    Update moderation.py

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -7,11 +7,20 @@ class moderation(commands.Cog):
     
     @app_commands.command(name="ban", description="Banishes the offender permanently from the guild.")
     @app_commands.guilds(919846932459974686)
-    async def ban(self, interaction: Interaction):
+    async def ban(self, interaction: Interaction, member: Member, *, reason: str):
+        embed = banEmbed()
+        embed.set_author(name=interaction.user.name, icon_url=interaction.user.avatar)
+        embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/919847648758992927/953452806872911892/the_nest.jpg")
+        embed.add_field(name="Offender Name,", value=member.name, inline=False)
+        embed.add_field(name="Offender ID,", value=member.id, inline=False)
+        embed.add_field(name="Ban Reason,", value=f'{reason}', inline=False)
+        
         if interaction.guild.owner():
             await interaction.guild.ban(user=member)
+            await interaction.response.send_message=(embed=embed)
         elif interaction.user.get_role(953018980522672148):
             await interaction.guild.ban(user=member)
+            await interaction.response.send_message=(embed=embed)
         else:
             await interaction.response.send_message('Insufficient role!')
    
