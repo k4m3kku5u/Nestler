161265c347aa6176e9436be193841c66bdca6848
k4m3kku5u <70122910+k4m3kku5u@users.noreply.github.com>
2022-03-22 22:48:10 -0500

    Update moderation.py

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -1,9 +1,19 @@
-from discord import app_commands
+from discord import app_commands, Interaction
 from discord.ext import commands
 
 class moderation(commands.Cog):
     def __init__(self, client) -> None:
         self.client = client
-
+    
+    @app_commands.command(name="ban", description="Banishes the offender permanently from the guild.")
+    @app_commands.guilds(919846932459974686)
+    async def ban(self, interaction: Interaction):
+        if interaction.guild.owner():
+            await interaction.guild.ban(user=member)
+        elif interaction.user.get_role(953018980522672148):
+            await interaction.guild.ban(user=member)
+        else:
+            await interaction.response.send_message('Insufficient role!')
+   
 async def setup(client):
     await client.add_cog(moderation(client))
