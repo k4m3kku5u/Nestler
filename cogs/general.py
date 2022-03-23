098228826b9c424aaf9d8a1f9ffcf45251c56ab5
k4m3kku5u <70122910+k4m3kku5u@users.noreply.github.com>
2022-03-23 17:34:54 -0500

    Update general.py

--- cogs/general.py
+++ cogs/general.py
@@ -1,9 +1,22 @@
-from discord import app_commands
+from datetime import datetime
+from discord import app_commands, Interaction
 from discord.ext import commands
+from utils.embeds import pingEmbed
 
 class general(commands.Cog):
     def __init__(self, client) -> None:
         self.client = client
+    
+    @app_commands.command(name="ping", description="")
+    @app_commands.guilds()
+    async def ping(self, interaction: Interaction)
+        embed = pingEmbed()
+        embed.set_author(name=f'{interaction.user.name}', icon_url=interaction.user.avatar)
+        embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/919847648758992927/953452806872911892/the_nest.jpg")
+        embed.add_field(name="CLIENT,", value=f"{round(self.client.latency * 1000)}ms", inline=True)
+        embed.add_field(name="DAPI,", value=None, inline=True)
+        embed.set_footer(text="Brought to you by: The Nest HQ.")
+        await interaction.response.send_message(embed=embed)
 
 async def setup(client):
     await client.add_cog(general(client))
